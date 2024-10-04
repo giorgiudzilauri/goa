8981042ebb4feaa0b6ed2dae38f20cdf4707ce33
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-10-04 18:53:44 +0400

    day9

---
+++ day 2/sashinao/homework.py
@@ -0,0 +1,10 @@
+name="Giorgi"
+surname="Udzilauri"
+age=13
+height=152
+
+knows_proggamin=True
+
+
+
+print(f"my name is {name} mu surname i am {age} yo my haight")
---
+++ day 3/home work/homework.py
@@ -0,0 +1,16 @@
+score=(200)
+health=(100)
+print("my core is" +str(score) +"my health is" +str(health))
+attacking_points=(50)
+defence_points=(20)
+print("my attacking points is" +str(attacking_points) + "my defence points is"+str(defence_points)+"")
+maths_core=75
+english_core=50
+science_core=50
+chemistry_core=20
+history_score=70
+print(f"my mats score is{maths_core} my english score is{english_core}my science score is {science_core} my chemistry score is{chemistry_core}my history score is{history_score}")
+name=("Giorgi")
+surname=("Udzilauri")
+age=(13)
+print(f"my name is {name} my surname is {surname} and my age is {age} ")
