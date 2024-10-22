e027160eae6a01c0b29b2e9210e7d4b10c05e6cd
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-10-22 21:51:11 +0400

    day 14

---
+++ day 8/class work/magic.py
@@ -0,0 +1,57 @@
+# data types boolean , int, float, str
+# bool True / False
+#-----------------------------------
+# math operators + // % - / * **
+#        1     2 
+# comparators 
+# >
+# < 
+# >=
+# <=
+# == უდრის
+# != არ უდრის
+
+#print( int(True)), (int(False))
+print(2 != 3)
+print(2 == 3 < 3 == 3)
+
+print( 2 < 3 )
+print( 2 < 5 )
+
+print( 3 > 2 )
+print( 2 > 1 )
+
+print( 3 >= 2 )
+print( 4 >= 3 )
+
+print( 3 <= 4 )
+print( 2 <= 4 )
+
+print( 4 == 4 ) 
+print( 2 == 2 )
+
+print( 2 != 3 )
+print( 3 != 4 )
+
+grisha = 10
+misha = 20
+print( grisha < misha)
+
+# logical operators შეუძლია შეადაროს ლოგიკური ოპერატორები საბოლოოდ კი განსაზღვრავს
+# bollean type
+# and - და
+# or - ან
+# not - არა
+# თუ სადმე არის და-ში False მაშინ ეს მოქმედება არის False
+
+#------------------------------------------------------------------------------------------------
+print ("------------")
+print(True and False and True)
+
+
+print(True and True) # True
+print(True and False) # False
+print(False and False) # False
+print(1 and 2 ) # False
+
+# or არ არის აუცილებელი 
