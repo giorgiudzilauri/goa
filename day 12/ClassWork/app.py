531ece97e385f01b610ca7429ca41fb0fbdd9496
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-10-27 16:06:08 +0400

    day 15

---
+++ day 12/ClassWork/app.py
@@ -0,0 +1,5 @@
+num = 0
+
+while num < :
+    print("")
+    num += 1
---
+++ day 15/HomeWork/app.py
@@ -0,0 +1,39 @@
+#davaleba
+elements = []
+
+elements.append(input("enter your name: "))
+elements.append(input("enter your age: "))
+elements.append(input("enter your city:"))
+elements.append(input("whats your fav color: "))
+
+
+elements.append("Additional information")
+
+for element in elements:
+    print(element)
+
+#davaleba_2
+numbers =[9, 5, 94, 711, 52, 96, 71, 8]
+smallest_number = numbers[0]
+
+
+for num in numbers:
+
+    if num < smallest_number:
+
+        smallest_number = num
+
+print("the smallest number:", smallest_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
