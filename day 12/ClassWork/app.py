b3cf038943306e4aa91d7ca61ac32d78841fcba4
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-10-27 18:44:48 +0400

    day 15

--- day 12/ClassWork/app.py
+++ day 12/ClassWork/app.py
@@ -1,5 +1,17 @@
-num = 0
+number = int(input("enter a number"))
 
-while num < :
-    print("")
-    num += 1
+if(number % 2) == 0:
+    print("{0} is even number" .format(number))
+else:
+    print("{0} is odd number" .format(number))
+
+#davaleba_2
+number2 = int(input("enter number"))
+if(number2 % 2) == 0:
+
+
+
+    
+    print("{0} is odd number" .format(number))
+else:
+    print("{0} is even number" .format(number))
