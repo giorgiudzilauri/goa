59aadf8d3fbf583401171e0b0ad40663b907d079
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-11-24 08:24:27 -0800

    day23

---
+++ day 17/HomeWork/lesson.py
@@ -0,0 +1,29 @@
+favorite_fruits = ["ანანასი", "საზამთრო", "ჟოლო", "მოცხარი", "მაყვალი"]
+
+print("პირველი ხილი:", favorite_fruits[0])
+
+print("ბოლო ხილი:", favorite_fruits[-1])
+
+numbers = list(range(1, 11))
+
+
+print("პირველი ელემენტი:", numbers[0])  
+print("მეხუთე ელემენტი:", numbers[4])   
+print("მეათე ელემენტი:", numbers[9])    
+
+animals = ["ვეფხვი", "ანტილოპა", "ჟირაფი", "ზებრა"]
+
+
+animals[0] = "ლომი"
+
+
+print("ახალი სია:", animals)
+
+favorite_movies = ["ინტერნეითი", "ჰელადოს", "ნატვრის ხე", "ხიდი ტერაბიტიაში"]
+
+
+print("საბოლლო ფილმი:", favorite_movies[-1])
+numbers = [10, 20, 30, 40, 50, 60]
+
+
+print("უკანასკნელი ორი ელემენტი:", numbers[-2:], )
