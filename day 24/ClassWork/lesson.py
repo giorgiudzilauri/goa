bc16f7c44bcf1099e1972a7c48a434156ec1d7eb
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-11-26 09:28:49 -0800

    day24

---
+++ day 24/ClassWork/lesson.py
@@ -0,0 +1,23 @@
+#task 1
+
+
+name = input("enter Your name: ")
+
+message =  "{} is good and cleaver boy!".format(name)
+
+print(message)
+
+#saklaso
+
+print("-----------------------------------split-----------------------------------")
+_str = "თეთრი თრითინა თეთრ თრთვილზე თრთოდა!"
+_split_str =_str.split( " " )
+print(_split_str)
+
+#task 2
+
+str = "shaurma ar aris xinkalze kargi".split( " " )
+
+joined = "! ".join(str)
+
+print(joined)
