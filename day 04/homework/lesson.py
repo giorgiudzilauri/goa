ac4b07b231b1bf3f6eb99f6b1a6b9cc3956e6e70
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-11-29 07:29:57 -0800

    day25

---
+++ day 04/homework/lesson.py
@@ -0,0 +1,42 @@
+#HomeWork 1
+name = input( "enter your name: ")
+last_name = input("enter your last name: ") 
+age = input("enter your age: ")
+
+print("hello",name)
+
+#HomeWork 2
+first = input("enter any number: ")
+second = input("enter any number: ")
+
+print("50 + 50")
+
+#HomeWork 3
+user1 = input("enter your name: ")
+print(user1)
+#არასწორე იყო usre1 და input ზე არ ქონდა ფრჩხილები
+
+#HomeWork 4
+math = (20 + 20)
+math2 = math - 10
+math3 = math * 2
+print(math3)
+#აქ შეცდომა იყო როგორც მესამე დავალებაზე ისე print არ ქონდდა ()
+
+#HomeWork 5
+academy = input("enter your academy name: ")
+print(academy)
+school = input("enter your school you go in to: ")
+print(school)
+name = input("enter your name: ")
+print(name)
+lastname = input("enter your lastname: ")
+print(lastname)
+age = input("enter your age: ")
+print(age)
+
+name = input( "enter your name: ")
+last_name = input("enter your last name: ") 
+age = input("enter your age: ")
+
+print("hello",name)
