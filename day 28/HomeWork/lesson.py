ffd596f4365b4daddd3b999fb7180e1ccc17e3ed
giorgiudzilauri <giorgiudzilauri61@gmail.com>
2024-12-13 10:35:36 -0800

    day29

--- day 28/HomeWork/lesson.py
+++ day 28/HomeWork/lesson.py
@@ -1,1 +1,24 @@
 #We have a task for you in Codewars
+
+#problem 1
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
+
+#problem 2
+def remove_char(s):
+    return s[1:-1]
+
+#problem 3
+def string_to_number(s):
+    return int(s)
+
+#problem 4
+def no_space(s):
+    return s.replace(" ", "")
+
+#problem 5
+def sum_array(arr):
+    return sum(arr)
